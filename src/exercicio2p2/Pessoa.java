c634d75b63756226c25f958ca4df7d8906616026
Axl886 <victorpereira8889@gmail.com>
2022-04-03 18:07:37 -0300

    feat: criação de classe abstrata Pessoa com método construtor.

---
+++ src/exercicio2p2/Pessoa.java
@@ -0,0 +1,11 @@
+package exercicio2p2;
+
+public abstract class Pessoa {
+	String nome;
+	int idade;
+	
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+   }
