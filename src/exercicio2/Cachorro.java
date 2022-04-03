efe2fc1732117d9ba959b7ae327a9d60eae3378e
Axl886 <victorpereira8889@gmail.com>
2022-04-03 16:52:18 -0300

    feat: criação de classe abstrata Cachorro com construtor e método falar()

---
+++ src/exercicio2/Cachorro.java
@@ -0,0 +1,11 @@
+package exercicio2;
+
+public class Cachorro extends Animal {
+	
+	public Cachorro(String nome, String raca) {
+		super(nome, raca);
+		}
+	public void falar() {
+		System.out.println("Au, auuu");
+		}
+	}
