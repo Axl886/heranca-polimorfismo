d5395982547e3aca06083779712b6841f97b896d
Axl886 <victorpereira8889@gmail.com>
2022-04-03 18:06:22 -0300

    feat: criação de classe filha Miseravel com método construtor e método Mendiga() para imprimir a situação financeira

---
+++ src/exercicio2p2/Miseravel.java
@@ -0,0 +1,13 @@
+package exercicio2p2;
+
+public class Miseravel extends Pessoa {
+
+	public Miseravel(String nome, int idade) {
+		super(nome, idade);
+	}
+
+	public void Mendiga() {
+		System.out.println("|Você se encontra em uma situação vulnerável e difícil.|");
+	}
+		
+}
