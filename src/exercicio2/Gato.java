d36593ca3aac40191840166c34a58165d198b6be
Axl886 <victorpereira8889@gmail.com>
2022-04-03 16:54:00 -0300

    feat: criação da classe Gato filha com construtor e método falar()

---
+++ src/exercicio2/Gato.java
@@ -0,0 +1,13 @@
+package exercicio2;
+
+public class Gato extends Animal{
+
+	public Gato(String nome, String raca) {
+		super(nome, raca);
+		}
+	public void falar() {
+		System.out.println("Miau,miauu");
+		}
+	}
+
+
