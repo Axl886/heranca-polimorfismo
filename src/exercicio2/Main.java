8810724efbfc1a94525d3756baf75bf67d75efb0
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-03 18:59:56 -0300

    fix: add ler.close to stop leaked memory

--- src/exercicio2/Main.java
+++ src/exercicio2/Main.java
@@ -9,21 +9,20 @@ public class Main {
 		Cachorro dog = new Cachorro(" ", " ");
 		Scanner ler = new Scanner(System.in);
 		String name;
-		
+
 		System.out.println("Qual seu animal preferido, gato ou cachorro?");
 		System.out.println(" | Digite Gato ou Cachorro | ");
-			name = ler.next();
-			
-		if(name.equals("Gato")) {
+		name = ler.next();
+
+		if (name.equals("Gato")) {
 			System.out.println("Seu animal preferido fala:");
 			cat.falar();
-		} else if(name.equals("Cachorro")) {
+		} else if (name.equals("Cachorro")) {
 			System.out.println("Seu animal preferido fala: ");
 			dog.falar();
-		}else {
-			System.out.println("Nome não reconhecido :/, tente novamente usando Gato ou Cachorro.");
+		} else {
+			System.out.println("Nome nï¿½o reconhecido :/, tente novamente usando Gato ou Cachorro.");
 		}
-	  }
+		ler.close();
 	}
-
-
+}
--- src/exercicio2p2/Main.java
+++ src/exercicio2p2/Main.java
@@ -9,30 +9,30 @@ public class Main {
 		Rica rica = new Rica(" ", 0);
 		Miseravel mise = new Miseravel(" ", 0);
 		Scanner ler = new Scanner(System.in);
-		
+
 		String name;
 		int age;
 		float renda;
-		
+
 		System.out.println("Digite seu nome");
-			name = ler.next();
+		name = ler.next();
 		System.out.println("Digite sua idade");
-			age = ler.nextInt();
-		System.out.println("Digite também sua renda mensal em reais. Exemplo: 1100,5300 etc.");
-			renda = ler.nextFloat();
-		
-			if(999 > renda){
-				System.out.println("Querido(a) "+name+" de "+age+" anos, recomendo uma mudança drástica de vida, ou procura ajuda. ");
-				mise.Mendiga();
-			} else if (4999 > renda) {
-				System.out.println("Querido(a) "+name+" de "+age+" anos, apesar de não estar totalmente livre de dívidas, ainda tem um padrão de vida aceitável. ");
-				pobre.trabalha();
-			}
-		      else if (renda > 5000) {
-		    	System.out.println("Querido(a) "+name+" de "+age+" anos, você está com ótima aquisição financeira ");  
-		    	rica.fazCompras();
-		   }
+		age = ler.nextInt();
+		System.out.println("Digite tambï¿½m sua renda mensal em reais. Exemplo: 1100,5300 etc.");
+		renda = ler.nextFloat();
+
+		if (999 > renda) {
+			System.out.println(
+					"Querido(a) " + name + " de " + age + " anos, recomendo uma mudanï¿½a drï¿½stica de vida, ou procura ajuda. ");
+			mise.Mendiga();
+		} else if (4999 > renda) {
+			System.out.println("Querido(a) " + name + " de " + age
+					+ " anos, apesar de nï¿½o estar totalmente livre de dï¿½vidas, ainda tem um padrï¿½o de vida aceitï¿½vel. ");
+			pobre.trabalha();
+		} else if (renda > 5000) {
+			System.out.println("Querido(a) " + name + " de " + age + " anos, vocï¿½ estï¿½ com ï¿½tima aquisiï¿½ï¿½o financeira ");
+			rica.fazCompras();
 		}
-     }
-
-
+		ler.close();
+	}
+}
