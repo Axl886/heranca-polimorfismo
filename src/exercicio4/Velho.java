6159f917aa5eb35bfab385232d66bd274aae8748
Victor Pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-14 20:33:17 -0300

    style: remove non-sense print

--- src/exercicio4/Velho.java
+++ src/exercicio4/Velho.java
@@ -14,7 +14,6 @@ public class Velho extends Imovel {
    */
   public void imprimirDesconto() {
     this.setPreco(this.getPreco() * 0.85f);
-    System.out.println("Desconto: 2500");
     System.out.println("Preço com desconto: " + this.getPreco());
   }
 }
