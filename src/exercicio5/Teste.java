443e146dd3c14beb9065763331650279309f0120
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-14 00:20:16 -0300

    style: move exercicioUm to a view

--- src/exercicio5/Teste.java
+++ src/exercicio5/Teste.java
@@ -1,35 +1,7 @@
 package exercicio5;
 
-import exercicio1.AssistenteAdministrativo;
-import exercicio1.AssistenteTecnico;
-
 public class Teste {
-  public static void exercicionUm() {
-    System.out.println("Exercício 1");
-
-    // Criando um assistente administrativo
-    AssistenteAdministrativo admin = new AssistenteAdministrativo();
-    admin.setMatricula(3130456);
-    admin.setNome("André Félix");
-    admin.setIdade(20);
-    admin.setSalario(20000);
-    admin.setTurno("Noturno");
-    admin.setAdicionalNoturno(2500);
-    admin.exibirDados();
-    System.out.print("\n");
-
-    // Criando assistente técnico
-    AssistenteTecnico tecnico = new AssistenteTecnico();
-    tecnico.setMatricula(8427679);
-    tecnico.setNome("Gol D. Roger");
-    tecnico.setIdade(17);
-    tecnico.setSalario(17500);
-    tecnico.setBonusSalario(4000);
-    tecnico.exibirDados();
-    System.out.print("\n");
-  }
-
   public static void main(String[] args) {
-    exercicionUm();
+    ExercicioUmView.main();
   }
 }
