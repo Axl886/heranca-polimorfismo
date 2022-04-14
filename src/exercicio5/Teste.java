96b5d7c1892775102aa5e9f67c827ca61669ef9a
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-04-14 11:33:56 -0300

    feat: add menu to interact with others exercises

--- src/exercicio5/Teste.java
+++ src/exercicio5/Teste.java
@@ -1,7 +1,53 @@
 package exercicio5;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Teste {
   public static void main(String[] args) {
-    ExercicioUmView.main();
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      int option = 0;
+      try {
+        System.out.println("Menu de opções");
+        System.out.println("1 - Exercício 1");
+        System.out.println("2 - Exercício 2");
+        System.out.println("3 - Exercício 3:");
+        System.out.println("4 - Exercício 4:");
+        System.out.println("5 - Sair");
+        option = scanner.nextInt();
+      } catch (InputMismatchException e) {
+        scanner.nextLine();
+        System.out.println("Valor não numérico");
+        continue;
+      }
+
+      switch (option) {
+        case 1:
+          ExercicioUmView.main();
+          break;
+
+        case 2:
+          System.out.println("Soon!");
+          break;
+
+        case 3:
+          System.out.println("Soon!");
+          break;
+
+        case 4:
+          ExercicioQuatroView.main();
+          break;
+
+        case 5:
+          scanner.close();
+          System.out.println("Tenha um dia excelente!");
+          System.exit(0);
+
+        default:
+          System.out.println("Opção não reconhecida");
+          break;
+      }
+    }
   }
 }
